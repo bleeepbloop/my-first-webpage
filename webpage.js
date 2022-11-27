1a9f19abf17c9322d974ede5490e72d3ebae68e2
bleeepbloop <slings.legging.0f@icloud.com>
2022-11-27 18:57:22 +0000

    my-first-webpage/

---
+++ webpage.js
@@ -0,0 +1,10 @@
+// let button = document.querySelector("#button");
+// button.addEventListener("click", function() {
+//     alert("hej");
+// });
+
+let button = document.querySelector("#button");
+button.addEventListener("click", function () {
+    button.textContent("added to favourites");
+    alert("added to favourites");
+  });
